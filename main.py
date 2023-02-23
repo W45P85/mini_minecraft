3c2413c0a2ff7fb33ec7f417761c47a225b37a7e
W45P85 <87259727+W45P85@users.noreply.github.com>
2023-02-23 09:25:29 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+# https://www.ursinaengine.org/documentation.html
+
+
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+app = Ursina()
+player = FirstPersonController()
+Sky()
+
+#speichert blöcke in einer liste
+boxes = []
+
+# zufallsfarben für blöcke
+def random_color():
+    red = random.Random().random() * 255
+    green = random.Random().random() * 255
+    blue = random.Random().random() * 255
+    return color.rgb(red, green, blue)
+
+# hinzufügen von blöcken
+def add_box(position):
+    boxes.append(
+        Button(
+        parent=scene,
+        model='cube',
+        origin=0.5,
+        color=random_color(),
+        position=position,
+        texture='grass'
+      )
+    )
+
+# ausgangswelt
+for x in range(20):
+  for y in range(20):
+    add_box( (x, 0, y) )
+
+# funktionen der linken und rechten maustaste
+def input(key):
+    for box in boxes:
+        if box.hovered:
+            if key == "left mouse down":
+                add_box(box.position + mouse.normal)
+            if key == "right mouse down":
+                boxes.remove(box)
+                destroy(box)
+
+# start
+if __name__ == '__main__':
+    app.run()
